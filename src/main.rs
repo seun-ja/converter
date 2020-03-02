023a7b55cab370cd8047b28b959cbc38cdbbedb4
seun <34725212+seun-ja@users.noreply.github.com>
2020-03-02 19:04:39 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,81 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(proc_macro_hygiene, decl_macro, plugin)]
+
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+extern crate serde_json;
 
-use rocket::response::content::Json;
-use std::io;
-use rocket::request::Form;
+use rocket::request::Request;
+use std::collections::HashMap;
 use rocket::http::RawStr;
+use rocket::response::status;
+use rocket::response::{Responder, Response};
+use rocket::http::{ContentType, Status};
+use rocket_contrib::json::{Json, JsonValue};
+use std::io;
+use rocket_contrib::templates::Template;
+use rocket::response;
+use rocket::data::FromData;
+// use chrono::{DateTime, TimeZone, Utc};
 
-// use std::ops::Mul;
 
-#[get("/", format = "json")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[derive(Debug)]
+struct ApiResponse {
+    json: JsonValue,
+    status: Status,
 }
 
-#[get("/hello?wave&<name>")]
-fn hello(name: String) -> String {
-    format!("Hello, {}!", name.as_str())
+impl<'r> Responder<'r> for ApiResponse {
+    fn respond_to(self, req: &Request) -> response::Result<'r> {
+        Response::build_from(self.json.respond_to(&req).unwrap())
+            .status(self.status)
+            .header(ContentType::JSON)
+            .ok()
+    }
 }
 
-#[derive(Debug, PartialEq, FromForm)]
+#[post("/converted", format = "application/json", data = "<data>")]
+fn home(data: Json<Input>) -> ApiResponse {
+    let mut errors: Vec<String> = vec![];
+
+    if errors.len() > 0 {
+        let error_response_json = json!({ "errors": errors });
+        ApiResponse {
+            status: Status::BadRequest,
+            json: error_response_json,
+        }
+    } else {
+        ApiResponse {
+            status: Status::Created,
+            json: json!({"status": "ok"}),
+        }
+    }
+}
+
+#[get("/")]
+fn index() -> Template {
+    let context: HashMap<String, String> = HashMap::new();
+    Template::render("index", &context)
+}
+
+#[derive(Debug, FromForm)]
 struct Input {
-    value: u32
+    degree: u32
 }
-
-// #[post("/submit", data = "<user_input>")]
-// fn submit_task(user_input: Form<UserInput>) -> String {
-//     format!("Your value: {}", user_input.value)
-// }
 
 impl Input {
     fn degree_converter(&self) -> f32 {
-        (self.value - 32) as f32 * 0.556
+        (self.degree - 32) as f32 * 0.556
     
         // self.new
     }
 }
 
-// #[post("/submit", data = "<user_input>")]
-// fn degree_converter(user_input: Form<UserInput>) -> f32 {
-//     (user_input.value - 32) as f32 * 0.556
-
-//     // self.new
-// }
-
 fn main() {
-    rocket::ignite().mount("/hello", routes![hello]).launch();
+    rocket::ignite()
+        .mount("/", routes![index,home])
+        .attach(Template::fairing())
+        .launch();
 
     let mut input = String::new();
 
@@ -53,8 +84,9 @@ fn main() {
 
     let input: u32 = input.trim().parse().expect("Please input interger");
 
-    let fahrenheit = Input {value: input};
+    let fahrenheit = Input {degree: input};
 
     print!("fahrenheit = {}", fahrenheit.degree_converter());
 }
+
 
